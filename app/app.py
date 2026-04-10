117fd9ee51f9433e61ddb8af0fb5f2dadfea6bc3
Varun <varun1234vi@gmail.com>
2026-04-10 17:01:49 +0530

    f

--- app/app.py
+++ app/app.py
@@ -22,7 +22,7 @@ def home():
     start = time.time()
     REQUEST_COUNT.labels(method='GET', endpoint='/').inc()
     REQUEST_LATENCY.labels(endpoint='/').observe(time.time() - start)
-    return jsonify({"message": "Hello, Flask with Prometheus! and boom in the devops i make the changement "})
+    return jsonify({"message": "Hello, Flask with Prometheus! "})
 
 @app.route('/metrics')
 def metrics():
